759c254a481bafa2de36dc5a0653e601a7b326fc
Adam Młynarski <infinite.state.machine@protonmail.com>
2018-12-29 01:09:30 +0100

    use smart pointers
    tesseract::TessBaseAPI actually has a destructor that calls End(), so we
    can use plain std::unique_ptr. For leptonica's Pix structure, we need a
    custom smart pointer (it can be made generic later on).

--- main.cpp
+++ main.cpp
@@ -1,28 +1,64 @@
 #include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
+#include <memory>
+#include <iostream>
+
+class pix_wrapper {
+public:
+    explicit pix_wrapper(Pix *pix);
+
+    virtual ~pix_wrapper();
+
+    operator Pix *();
+
+    Pix *operator->();
+
+    // Prohibit copying and assignment.
+    pix_wrapper(const pix_wrapper &) = delete;
+
+    pix_wrapper &operator=(const pix_wrapper &) = delete;
+
+private:
+
+    Pix *pix;
+};
 
 int main()
 {
-    char *outText;
+    std::unique_ptr<tesseract::TessBaseAPI> api{new tesseract::TessBaseAPI()};
 
-    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
     // Initialize tesseract-ocr with English, without specifying tessdata path
-    if (api->Init(NULL, "eng")) {
+    if (api->Init(nullptr, "eng")) {
         fprintf(stderr, "Could not initialize tesseract.\n");
         exit(1);
     }
 
     // Open input image with leptonica library
-    Pix *image = pixRead("/usr/src/tesseract/testing/phototest.tif");
+    pix_wrapper image{pixRead("phototest.tif")};
     api->SetImage(image);
-    // Get OCR result
-    outText = api->GetUTF8Text();
-    printf("OCR output:\n%s", outText);
 
-    // Destroy used object and release memory
-    api->End();
-    delete [] outText;
-    pixDestroy(&image);
+    // Get OCR result
+    std::unique_ptr<char[]> outText{api->GetUTF8Text()};
+    printf("OCR output:\n%s", outText.get());
 
     return 0;
+}
+
+pix_wrapper::pix_wrapper(Pix *pix) : pix(pix)
+{
+}
+
+pix_wrapper::~pix_wrapper()
+{
+    pixDestroy(&pix);
+}
+
+pix_wrapper::operator Pix *()
+{
+    return pix;
+}
+
+Pix *pix_wrapper::operator->()
+{
+    return pix;
 }
